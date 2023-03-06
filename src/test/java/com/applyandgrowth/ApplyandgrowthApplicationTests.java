c8f487d12628728eec4e19767ecefd52b883fd88
Felipe Piassa <felipepiassa1802@gmail.com>
2023-03-05 23:17:35 -0300

    files

---
+++ src/test/java/com/applyandgrowth/ApplyandgrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applyandgrowth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyandgrowthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
