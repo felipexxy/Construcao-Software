76e7eb8800cb360aef368c851f8d5bd1a2a4837f
ArthurBelini <ra118999@uem.br>
2023-03-13 21:20:44 -0300

    Adição de Login

--- src/main/java/com/applyandgrowth/controllers/ClientController.java
+++ src/main/java/com/applyandgrowth/controllers/ClientController.java
@@ -1,5 +1,7 @@
 package com.applyandgrowth.controllers;
 
+import java.sql.*;  
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -36,9 +38,56 @@ public class ClientController {
 		return "redirect:/createAccount"; 
 	}
 	
-	@RequestMapping(value="/")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String login() {
 		return "login";
+	}
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String login(@Valid Client client, BindingResult br, RedirectAttributes attributes) {
+		String validationResult = this.authenticateUser(client.getEmail(), client.getPassword());
+
+		if(validationResult.equals("0")) {
+			return "redirect:/indexClient";
+		} else if(validationResult.equals("1")) {
+			return "redirect:/indexAdvertiser";
+		}
+		
+		attributes.addFlashAttribute("flashMessage", "Wrong email or password!");
+		attributes.addFlashAttribute("flashType", "danger");
+		return "redirect:/"; 
+	}
+
+	private String authenticateUser(String email, String password) {
+		String authenticationResult;
+
+		try {  
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/applyandgrowth", "root", "2358");  
+			Statement stmt = con.createStatement();  
+
+			String authenticationQuery = "select * from client where email = '" +
+				email + "' and password = '" + password + "'";
+			System.out.println(authenticationQuery);
+
+			ResultSet rs=stmt.executeQuery(authenticationQuery);  
+
+			if(rs.next()) {
+				authenticationResult =  rs.getString(4);  // valor de isAdvertiser
+			} else {
+				authenticationResult = "false";
+			}
+
+			rs.close();
+			
+			return authenticationResult;
+		} catch(Exception e) {
+			System.out.println("Erro: " + e);
+			System.exit(1);
+		}
+
+		return "exception";
 	}
 	
 	@RequestMapping(value="/recoverPass", method=RequestMethod.GET)
@@ -55,5 +104,4 @@ public class ClientController {
 	public String indexAdvertiser() {
 		return "index_advertiser";
 	}
-	
 }
